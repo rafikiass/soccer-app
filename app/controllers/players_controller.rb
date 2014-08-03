0756bd449f5e64e915ac44014c3c367b12c16416
Trevor Jones <trevordavidjones@gmail.com>
2014-08-03 15:39:34 -0500

    added styling

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,9 +1,10 @@
 class PlayersController < ApplicationController
 before_action :authenticate_admin!, :only => [:destroy, :edit, :update, :create]
+
 def players
     
-    @players = Player.all
-    
+    @xmlplayers = Xmlsoccer::Client.new(api_key: 'VLQTPIPEUHYLQXSLUPZBDFGNGEPYPWSPTOIJIEJFUUTSWCLTKI', api_type: 'Demo')
+    @players = @xmlplayers.get_players_by_team(team_id: '45')
   end 
 
   def new
@@ -16,9 +17,13 @@ def players
     redirect_to '/players'
   end
 
+client = Xmlsoccer::Client.new(api_key: 'VLQTPIPEUHYLQXSLUPZBDFGNGEPYPWSPTOIJIEJFUUTSWCLTKI', api_type: 'Demo')
+# teams = client.get_all_teams_by_league_and_season(league: 'Scottish Premier League', season_date_string: '1415')
+
   def show
     @player = Player.friendly.find(params[:id])
-    
+    xmlplayer = Xmlsoccer::Client.new(api_key: 'VLQTPIPEUHYLQXSLUPZBDFGNGEPYPWSPTOIJIEJFUUTSWCLTKI', api_type: 'Demo')
+    @player = 
   end
 
   def edit
