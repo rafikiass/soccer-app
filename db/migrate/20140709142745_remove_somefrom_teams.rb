2fde66d8e7abdb4ed00d2e2f5cdb56a88acc1596
Trevor Jones <trevordavidjones@gmail.com>
2014-08-27 21:11:37 -0500

    dumped my data into seed file

--- db/migrate/20140709142745_remove_somefrom_teams.rb
+++ db/migrate/20140709142745_remove_somefrom_teams.rb
@@ -2,7 +2,7 @@ class RemoveSomefromTeams < ActiveRecord::Migration
   def change
     change_table :teams do |t|
       t.remove :first_established 
-      t.string :year_established
+      t.remove :year_established
     end
   end
 end
