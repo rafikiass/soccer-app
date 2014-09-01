3b537e484340e2863a26ef24e7be7474a082b08a
Trevor Jones <trevordavidjones@gmail.com>
2014-08-31 19:42:31 -0500

    fixing up show page error

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -21,8 +21,8 @@ class TeamsController < ApplicationController
     @schedule = Unirest.get("http://football-api.com/api/?Action=fixtures&APIKey=#{ENV['FOOTBALL_API']}&comp_id=#{@team.league.football_api_comp_id}&&from_date=05.08.2014&&to_date=05.08.2015",
                     headers:{"Accept" => "application/json"}).body
     @matches = @schedule["matches"]
-    @rank = Unirest.get("http://football-api.com/api/?Action=standings&APIKey=#{ENV['FOOTBALL_API']}&comp_id=#{@team.league.football_api_comp_id}",
-                    headers: {"Accept" => "application/json"}).body
+    @ranks = Unirest.get("http://football-api.com/api/?Action=standings&APIKey=#{ENV['FOOTBALL_API']}&comp_id=#{@team.league.football_api_comp_id}",
+                    headers: {"Accept" => "application/json"}).body["teams"]
     @headlines = Unirest.get("http://api.espn.com/v1/sports/soccer/#{@team.league.espn_shortname}/teams/#{@team.espn_team_id}/news?apikey=#{ENV['ESPN_KEY']}",
                     headers: {"Accept" => "application/json"}).body["headlines"]
     @sbnation = SimpleRSS.parse open("http://www.sbnation.com/rss/section/#{@team.league.sbnation_url}/index.xml")
